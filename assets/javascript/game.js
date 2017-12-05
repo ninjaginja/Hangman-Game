9e8c14e9503ae77d8e285e604f5d630f9d3dfb79
ninjaginja^C <mollywojcik@Mollys-MacBook-Pro-2.local>
2017-12-05 08:26:49 -0700

    pseudo code notes

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// Define all the letter options with an array
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+// Create set of answers with an array
+var answers = ["griswolds", "shitter was full", "station wagon", "cousin eddie", "tree sap"]
+
+// Set # of guesses counter equal to 12
+var guessCounter = 12;
+
+// Use for loop to call first answer, followed by new game when solved or # guesses reaches 0
+
+  // Display placeholders to account for each letter of the answer
+
+  // Detect user input upon keystroke
+
+  // Reduce # of guesses counter by one upon each new letter keystroke
+
+  // Check user input to determine if letter exists in selected answer
+
+  // Place used letter in ‘Letter Graveyard’ div
+
+
+
+
+
+
+
+// OLD CODE ...
+// var userInput = document.getElementById("user-input");
+// console.log(userInput);
+// document.onkeyup = function(event) {
+//   var userInput = document.getElementById("user-input");
+//   userInput.innerHTML = event.key;
+//   console.log(userInput);
+// };
