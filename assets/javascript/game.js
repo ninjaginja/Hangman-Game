8d3790bd14c093420f6025a2a70d6019f7a2dff1
ninjaginja^C <mollywojcik@Mollys-MacBook-Pro-2.local>
2017-12-06 19:02:40 -0700

    updated .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,44 @@
+window.onload = function() {
+
 // Define all the letter options with an array
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
 // Create set of answers with an array
-var answers = ["griswolds", "shitter was full", "station wagon", "cousin eddie", "tree sap"]
+var answers = ["shitter was full", "griswolds", "station wagon", "cousin eddie", "tree sap"];
 
 // Set # of guesses counter equal to 12
+
 var guessCounter = 12;
 
 // Use for loop to call first answer, followed by new game when solved or # guesses reaches 0
+// for (var i = 0; i < answers.length; i++) {
 
   // Display placeholders to account for each letter of the answer
+  for (var j = 0; j < answers[0].length; j++) {
+    console.log("_");
+    var placeholderDiv = document.createElement("div");
+    // console.log(placeholderDiv);
+    placeholderDiv.className = "letter-box";
+    // console.log(placeholderDiv);
+    var testVar = document.getElementById("answer").appendChild(placeholderDiv);
+
+  };
+
+
+
+
+  // };
+
+
+
+
+
+  // function insertPlaceholders(currentAnswer) {
+  //   currentAnswer.length
+  //
+  // };
+
+
 
   // Detect user input upon keystroke
 
@@ -33,3 +62,5 @@ var guessCounter = 12;
 //   userInput.innerHTML = event.key;
 //   console.log(userInput);
 // };
+
+};
